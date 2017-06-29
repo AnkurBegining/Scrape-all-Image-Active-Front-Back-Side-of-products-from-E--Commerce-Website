6a1b7b52c7124636d3a4148a2fd13a13a8193a50
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-06-29 17:05:47 +0545

    1.4: Download every image.

--- Ankur/scrapper.py
+++ Ankur/scrapper.py
@@ -1,4 +1,6 @@
+import os
 import time
+from urllib.request import urlopen as uReq
 
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
@@ -10,11 +12,9 @@ browser = webdriver.Firefox()
 myUrl = "https://www.titan.co.in/shop-online/watches/titan"
 browser.get(myUrl)
 
-
-
-for i in range(0, 50):
+for i in range(0, 60):
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(5)
+    time.sleep(1)
 
     try:
         browser.find_element_by_link_text("Show more products").click()
@@ -33,3 +33,41 @@ page_soup = soup(browser.page_source, "html5lib")
 
 containers = page_soup.find_all("div", {"class": "product"})
 print("Number of watches found:: ", len(containers))
+
+for contains in containers:
+    try:
+        product_page_link_container = contains.find('a', {'class': 'product_page_link'})
+        product_detail_url = product_page_link_container['href']
+        newUrl = myUrl + product_detail_url
+
+        print(newUrl)
+
+        openurl = uReq(newUrl)
+        pageHtml = openurl.read()
+        openurl.close()
+        pageSoup = soup(pageHtml, 'html.parser')
+
+        # Model Number
+        ContainersForModel = pageSoup.find('p', {'class': 'stock'}).getText()
+        ModelNumber = ContainersForModel.strip()
+        PrefectModelNumber = ModelNumber[1:ModelNumber.index(')')]
+
+        # Containers For Image
+        ContainersForImage = pageSoup.find_all('li', {'class': 'thumbnail_image'})
+
+        i = 0
+        Directory = "/home/ankur/PycharmProjects/Download all Image(Active,Front,Side,Back etc) of product from E-Commerce WebSite/Ankur/Image Container"
+        os.chdir(Directory)
+        for contains in ContainersForImage:
+            Image = contains.img['src']
+            imageName = PrefectModelNumber + "_" + str(i)
+            ImageFile = open(imageName + '.jpeg', 'wb')
+
+            ImageFile.write(uReq(Image).read())
+            ImageFile.close()
+            i = i + 1
+        i = 0
+    except:
+        pass
+
+print("Ankur Ranjan")
