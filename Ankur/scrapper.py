0bf2d404ef3c57191e0977564b3038f94b0b912d
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-06-29 10:28:27 +0545

    1.1: Use Selenium to scroll down to the end. Once it reached to bottom, it will automatically click on Show More Product and scroll it more upto the real end.

--- Ankur/scrapper.py
+++ Ankur/scrapper.py
@@ -6,8 +6,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+# Open browser and get data
 browser = webdriver.Firefox()
 
 # Url from which i will scrap my data
 myUrl = "https://www.titan.co.in/shop-online/watches/titan"
 browser.get(myUrl)
+
+
+
+for i in range(0, 50):
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+
+    try:
+        browser.find_element_by_link_text("Show more products").click()
+    except:
+        pass
